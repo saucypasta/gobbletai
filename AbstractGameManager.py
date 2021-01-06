dda8d4e769f4b985085a5daa1afbaebe5b1a444c
Avi Rudich <avi.rudich@gmail.com>
2021-01-06 18:00:26 -0500

    added take_action to GameManager

--- AbstractGameManager.py
+++ AbstractGameManager.py
@@ -6,3 +6,7 @@ class Abstract_Game_Manager():
 		#returns True,w is state a terminal state
 		#w = 1 if win, -1 if loss
 		pass
+
+	def take_action(self,curr_state,action):
+		#returns the new state following the action taken from curr_state
+		pass
